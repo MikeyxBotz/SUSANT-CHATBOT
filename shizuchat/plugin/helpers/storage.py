4807a2de787b0953b76517a0e26a3d5adafd24fa
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2025-07-17 18:17:53 +0530

    Update storage.py

--- shizuchat/plugin/helpers/storage.py
+++ shizuchat/plugin/helpers/storage.py
@@ -2,7 +2,7 @@ import random
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 CHAT_STORAGE = [
-    "mongodb+srv://deepak-bot:LoRm7nrAqVs2xncW@cluster0.hnp8icx.mongodb.net/?retryWrites=true&w=majority",
+    "",
 ]
 
 BADMUNDA = MongoCli(random.choice(CHAT_STORAGE))
